53df9c61b0487eba47f04d754b135b1adffb4eae
Guy Yafe <guy@site-building.co.il>
2016-04-17 10:16:57 +0300

    Remove Redundant Code

--- guy-yafe-java/Euler-45-Solver/src/main/java/il/co/site_building/euler_45_solver/SimpleSolver.java
+++ guy-yafe-java/Euler-45-Solver/src/main/java/il/co/site_building/euler_45_solver/SimpleSolver.java
@@ -14,16 +14,15 @@ public class SimpleSolver {
     System.out.println("Start time: " + start);
 
     long MAX_VALUE_TO_CALCULATE = Long.parseLong(args[0]);
-    Map<Long, Long> foundPairs = new HashMap<>();
 
-    findEuler45(MAX_VALUE_TO_CALCULATE, foundPairs);
+    findEuler45(MAX_VALUE_TO_CALCULATE);
 
     long end = System.currentTimeMillis();
     System.out.println("End time: " + end);
     System.out.println("Calculation took: " + (end - start) + "millis");
   }
 
-  private static void findEuler45(long max, Map<Long, Long> foundPairs) {
+  private static void findEuler45(long max) {
     Map<Long, Long> triangleBuckets = new HashMap<>(((int) max) * 2);
     Map<Long, Long> pentagonalBuckets = new HashMap<>();
     for (long t = 0; t < max; t++) {
@@ -42,7 +41,7 @@ public class SimpleSolver {
       if (pentagonalBuckets.containsKey(hexagonal)) {
         System.out.println(
             "Found value: " + hexagonal + " with inidices: Triangle: " + triangleBuckets.get(hexagonal) +
-            " Pentagonal: " + foundPairs.get(hexagonal) + " Hexagonal: " + h);
+            " Pentagonal: " + pentagonalBuckets.get(hexagonal) + " Hexagonal: " + h);
       }
     }
   }
